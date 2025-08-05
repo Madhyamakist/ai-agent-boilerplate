cdce497f42c02340faf491e67c69887676c22851
Vivek Agarwal <62823320+singhalvivek@users.noreply.github.com>
2025-08-05 10:33:49 +0530

    LLM API and System Prompt (#4)
    * LLM API and System Prompt
    
    * Simple system prompt

--- code/app.py
+++ code/app.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask, render_template, request, jsonify
+from llm_api import get_groq_response
 from validators import validate_input
 from config import DEBUG
 
@@ -13,17 +14,26 @@ def chat():
     return render_template('chat.html')
 
 #Rendering response
-# chat_api is a Flask route function defined that acts as the backend API endpoint for chat exchanges. It is the API endpoint your front end calls to send user messages and receive chatbot responses.
+# chat_api is a Flask route function defined that acts as the backend API endpoint for chat exchanges. It is the API endpoint your frontend calls to send user messages and receive chatbot responses.
 # It receives a JSON request containing the user's chat input from the frontend, validates the input, sends the validated input to the LLM, and returns a JSON response.
+
 @app.route('/chat', methods=['POST'])
 def chat_api():
     data = request.get_json()
-    input = data.get('input', '').strip()
+    input = data.get('input', '')
+    # Input Validation
     is_valid, message = validate_input(input)
     if not is_valid:
         return jsonify({'success': False, 'error': message}), 400
-
-    return jsonify({'success': True, 'response': "Hello"})
+    # Get response from LLM
+    try:
+        bot_response = get_groq_response(input.strip())
+        return jsonify({'success': True, 'response': bot_response})
+    except Exception as e:
+        print(f"Error during LLM call: {e}")
+        return jsonify({
+            'success': False,
+            'error': "Sorry, something went wrong while processing your message. Please try again later."}), 500
 
 if __name__ == '__main__':
     app.run(debug=DEBUG)
