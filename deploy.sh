e245278f7f47d144cb063c56e99cd62d9c4eb555
Vivek Agarwal <viveksinghalp@gmail.com>
2025-08-27 03:18:48 +0530

    testing deploy.yml with deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,15 +2,33 @@
 set -e  # exit if any command fails
 
 echo "Pulling latest code..."
-git pull origin main
+cd ~
+cd ~/Ai-agent-boilerplate
+git pull origin deploy
 
 echo "Activating virtualenv..."
+cd ai-agent-boilerplate
 source venv/bin/activate
+
+echo "Setup .env file"
+cd code
+echo -n > .env
+cat <<EOF > .env
+DEBUG=True
+GROQ_API_KEY=${GROQ_API_KEY}
+GROQ_MODEL_NAME=meta-llama/llama-4-scout-17b-16e-instruct
+# PostgreSQL Database Configuration
+# Local PostgreSQL connection (adjust username/password as needed)
+DATABASE_URL=postgresql://postgres:${POSTGRES_PASSWORD}@localhost:5432/
+EOF
 
 echo "Installing dependencies..."
 pip install -r requirements.txt
 
 echo "Restarting service..."
-systemctl restart myapp
+pkill -f flask
+export FLASK_APP=app.py
+nohup flask run --host=0.0.0.0 --port=5000
+
 
 echo "✅ Deployment complete!"
