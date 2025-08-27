cfebb83c6b5bb772caa966a9922fd04c3d592631
Vivek Agarwal <viveksinghalp@gmail.com>
2025-08-27 11:27:26 +0530

    testing deploy.yml without ssh

--- update_app.sh
+++ update_app.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 set -e
-git config --global --add safe.directory /home/vivek/Ai-agent-boilerplate/ai-agent-boilerplate
 
+sudo -u vivek -i bash <<'EOF'
 cd /home/vivek/Ai-agent-boilerplate/ai-agent-boilerplate
-
-# pull latest code
 git fetch --all
-git reset --hard origin/deploy  # or origin/main depending on your branch
-
-# run your deployment script
+git reset --hard origin/deploy
 chmod +x deploy.sh
 ./deploy.sh deploy
+EOF
